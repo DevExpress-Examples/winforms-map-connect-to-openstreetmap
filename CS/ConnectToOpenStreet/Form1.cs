b0e9cddbee5ff42559f075c198234cf3517ac86d
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/ConnectToOpenStreet/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Forms;
+using DevExpress.XtraMap;
+
+namespace ConnectToOpenStreet {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            // Create a map control.
+            MapControl map = new MapControl();
+
+            // Specify the map position on the form.           
+            map.Dock = DockStyle.Fill;
+
+            // Add the map control to the window.
+            this.Controls.Add(map);
+
+            // Create an image tiles layer and add it to the map.
+            ImageTilesLayer tilesLayer = new ImageTilesLayer();
+            map.Layers.Add(tilesLayer);            
+
+            // Create an Open Street data provider.
+            OpenStreetMapDataProvider provider = new OpenStreetMapDataProvider();
+            tilesLayer.DataProvider = provider;
+
+        }
+    }
+}
---
+++ VB/ConnectToOpenStreet/Form1.vb
@@ -0,0 +1,33 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Windows.Forms
+Imports DevExpress.XtraMap
+
+Namespace ConnectToOpenStreet
+	Partial Public Class Form1
+		Inherits Form
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+			' Create a map control.
+			Dim map As New MapControl()
+
+			' Specify the map position on the form.           
+			map.Dock = DockStyle.Fill
+
+			' Add the map control to the window.
+			Me.Controls.Add(map)
+
+			' Create an image tiles layer and add it to the map.
+			Dim tilesLayer As New ImageTilesLayer()
+			map.Layers.Add(tilesLayer)
+
+			' Create an Open Street data provider.
+			Dim provider As New OpenStreetMapDataProvider()
+			tilesLayer.DataProvider = provider
+
+		End Sub
+	End Class
+End Namespace
