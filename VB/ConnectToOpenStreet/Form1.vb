ad6c4d48d0de7035fff9b34ad1717a89cb9ef31e
DevExpressExampleBot <fakeEmail>
2019-06-07 16:26:04 +0300

    Source auto update [skip ci]

--- VB/ConnectToOpenStreet/Form1.vb
+++ VB/ConnectToOpenStreet/Form1.vb
@@ -3,31 +3,39 @@ Imports System.Windows.Forms
 Imports DevExpress.XtraMap
 
 Namespace ConnectToOpenStreet
-    Partial Public Class Form1
-        Inherits Form
+	Partial Public Class Form1
+		Inherits Form
 
-        Public Sub New()
-            InitializeComponent()
-        End Sub
+		Public Sub New()
+			InitializeComponent()
+		End Sub
 
-        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
-            ' Create a map control.
-            Dim map As New MapControl()
+		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+			' Create a map control.
+			Dim map As New MapControl()
 
-            ' Specify the map position on the form.           
-            map.Dock = DockStyle.Fill
+			' Specify the map position on the form.           
+			map.Dock = DockStyle.Fill
 
-            ' Add the map control to the window.
-            Me.Controls.Add(map)
+			' Add the map control to the window.
+			Me.Controls.Add(map)
 
-            ' Create an image tiles layer and add it to the map.
-            Dim tilesLayer As New ImageLayer()
-            map.Layers.Add(tilesLayer)
+			' Create an image tiles layer and add it to the map.
+			Dim tileLayer As New ImageLayer()
+			map.Layers.Add(tileLayer)
 
-            ' Create an Open Street data provider.
-            Dim provider As New OpenStreetMapDataProvider()
-            tilesLayer.DataProvider = provider
+			' Create an Open Street data provider.
+			Dim provider As New OpenStreetMapDataProvider()
+			tileLayer.DataProvider = provider
 
-        End Sub
-    End Class
+			' Specify a template that is used to obtain image tiles. 
+			' provider.TileUriTemplate = "http://{subdomain}.tile.MyCustomOSMProvider.org/{tileLevel}/{tileX}/{tileY}.png";
+
+			AddHandler provider.WebRequest, AddressOf OnWebRequest
+		End Sub
+
+		Private Sub OnWebRequest(ByVal sender As Object, ByVal e As MapWebRequestEventArgs)
+			e.UserAgent = "XtraMap Getting Started - Connect to OpenStreetMap"
+		End Sub
+	End Class
 End Namespace
--- VB/ConnectToOpenStreet/Program.vb
+++ VB/ConnectToOpenStreet/Program.vb
@@ -4,19 +4,15 @@ Imports System.Linq
 Imports System.Windows.Forms
 
 Namespace ConnectToOpenStreet
-    Friend NotInheritable Class Program
-
-        Private Sub New()
-        End Sub
-
-        ''' <summary>
-        ''' The main entry point for the application.
-        ''' </summary>
-        <STAThread> _
-        Shared Sub Main()
-            Application.EnableVisualStyles()
-            Application.SetCompatibleTextRenderingDefault(False)
-            Application.Run(New Form1())
-        End Sub
-    End Class
+	Friend Module Program
+		''' <summary>
+		''' The main entry point for the application.
+		''' </summary>
+		<STAThread>
+		Sub Main()
+			Application.EnableVisualStyles()
+			Application.SetCompatibleTextRenderingDefault(False)
+			Application.Run(New Form1())
+		End Sub
+	End Module
 End Namespace
