fe21f1d14a51d722df2cd6b222679357d514780e
EugeniaSimonova <62024758+EugeniaSimonova@users.noreply.github.com>
2020-12-29 13:01:34 +0300

    Update Form1.cs

--- CS/ConnectToOpenStreet/Form1.cs
+++ CS/ConnectToOpenStreet/Form1.cs
@@ -29,7 +29,7 @@ namespace ConnectToOpenStreet {
             tileLayer.DataProvider = provider;
 
             // Specify a template that is used to obtain image tiles. 
-            //provider.TileUriTemplate = "http://{subdomain}.tile.MyCustomOSMProvider.org/{tileLevel}/{tileX}/{tileY}.png";
+            provider.TileUriTemplate = "http://{0}.tile.MyCustomOSMProvider.org/{1}/{2}/{3}.png";
 
             provider.WebRequest += OnWebRequest;
         }
