febe5bafdbc35dc44ccc57af8666efa2d67c0914
EugeniaSimonova <62024758+EugeniaSimonova@users.noreply.github.com>
2020-12-30 11:15:38 +0300

    Update Form1.cs

--- CS/ConnectToOpenStreet/Form1.cs
+++ CS/ConnectToOpenStreet/Form1.cs
@@ -9,22 +9,25 @@ namespace ConnectToOpenStreet {
         }
 
         private void Form1_Load(object sender, EventArgs e) {
-            // Create a map control.
+            // Create a Map Control object.
             MapControl map = new MapControl();
 
             // Specify the map position on the form.           
             map.Dock = DockStyle.Fill;
 
-            // Add the map control to the window.
+            // Add the Map Control to the form.
             this.Controls.Add(map);
 
             // Create an image tiles layer and add it to the map.
             ImageLayer tilesLayer = new ImageLayer();
             map.Layers.Add(tilesLayer);            
 
-            // Create an Open Street data provider.
+            // Create an Open Street Map data provider.
             OpenStreetMapDataProvider provider = new OpenStreetMapDataProvider();
             tilesLayer.DataProvider = provider;
+            
+            // Specify the TileUriTemplate property to connect the Map Control to an OSM server. 
+           provider.TileUriTemplate = "http://{0}.tile.MyCustomOSMProvider.org/{1}/{2}/{3}.png";
 
         }
     }
