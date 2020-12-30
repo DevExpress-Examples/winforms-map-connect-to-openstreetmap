435922df49237378f814db32281d3a001d40ef9d
DevExpressExampleBot <fakeEmail>
2020-12-30 11:21:07 +0300

    Source auto update [skip ci]

--- VB/ConnectToOpenStreet/Form1.vb
+++ VB/ConnectToOpenStreet/Form1.vb
@@ -3,34 +3,34 @@ Imports System.Windows.Forms
 Imports DevExpress.XtraMap
 
 Namespace ConnectToOpenStreet
-	Partial Public Class Form1
-		Inherits Form
+    Partial Public Class Form1
+        Inherits Form
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+        Public Sub New()
+            InitializeComponent()
+        End Sub
 
-		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
-			' Create a Map Control object.
-			Dim map As New MapControl()
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+            ' Create a map control.
+            Dim map As New MapControl()
 
-			' Specify the map position on the form.           
-			map.Dock = DockStyle.Fill
+            ' Specify the map position on the form.           
+            map.Dock = DockStyle.Fill
 
-			' Add the Map Control to the form.
-			Me.Controls.Add(map)
+            ' Add the map control to the window.
+            Me.Controls.Add(map)
 
-			' Create an image tiles layer and add it to the map.
-			Dim tilesLayer As New ImageLayer()
-			map.Layers.Add(tilesLayer)
+            ' Create an image tiles layer and add it to the map.
+            Dim tilesLayer As New ImageLayer()
+            map.Layers.Add(tilesLayer)
 
-			' Create an Open Street Map data provider.
-			Dim provider As New OpenStreetMapDataProvider()
-			tilesLayer.DataProvider = provider
+            ' Create an Open Street data provider.
+            Dim provider As New OpenStreetMapDataProvider()
+            tilesLayer.DataProvider = provider
+        
+            ' Specify the TileUriTemplate property to connect the Map Control to an OSM server. 
+            provider.TileUriTemplate = "http://{0}.tile.MyCustomOSMProvider.org/{1}/{2}/{3}.png"
 
-			' Specify the TileUriTemplate property to connect the Map Control to an OSM server. 
-		   provider.TileUriTemplate = "http://{0}.tile.MyCustomOSMProvider.org/{1}/{2}/{3}.png"
-
-		End Sub
-	End Class
+        End Sub
+    End Class
 End Namespace
