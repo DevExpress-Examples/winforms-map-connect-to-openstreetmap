ed1f13b0e41f33cce48967f3d049984a6a75d225
DevExpressExampleBot <fakeEmail>
2022-08-16 16:42:58 +0400

    Source auto update [skip ci]

--- VB/ConnectToOpenStreet/Form1.vb
+++ VB/ConnectToOpenStreet/Form1.vb
@@ -1,36 +1,31 @@
-﻿Imports System
+Imports System
 Imports System.Windows.Forms
 Imports DevExpress.XtraMap
 
 Namespace ConnectToOpenStreet
-    Partial Public Class Form1
+
+    Public Partial Class Form1
         Inherits Form
 
         Public Sub New()
             InitializeComponent()
         End Sub
 
-        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
-            ' Create a map control.
-            Dim map As New MapControl()
-
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs)
+            ' Create a Map Control object.
+            Dim map As MapControl = New MapControl()
             ' Specify the map position on the form.           
             map.Dock = DockStyle.Fill
-
-            ' Add the map control to the window.
+            ' Add the Map Control to the form.
             Me.Controls.Add(map)
-
             ' Create an image tiles layer and add it to the map.
-            Dim tilesLayer As New ImageLayer()
+            Dim tilesLayer As ImageLayer = New ImageLayer()
             map.Layers.Add(tilesLayer)
-
-            ' Create an Open Street data provider.
-            Dim provider As New OpenStreetMapDataProvider()
+            ' Create an Open Street Map data provider.
+            Dim provider As OpenStreetMapDataProvider = New OpenStreetMapDataProvider()
             tilesLayer.DataProvider = provider
-        
             ' Specify the TileUriTemplate property to connect the Map Control to an OSM server. 
             provider.TileUriTemplate = "http://{0}.tile.MyCustomOSMProvider.org/{1}/{2}/{3}.png"
-
         End Sub
     End Class
 End Namespace
